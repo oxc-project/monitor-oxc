351372df89b1999bd4d15e42327958e0d1f02580
github-action-benchmark <github@users.noreply.github.com>
2024-08-08 21:17:27 +0000

    add Oxc Metrics (customSmallerIsBetter) benchmark result for 5bd2790f694765be2aeda66c221c388103cc8f6c

--- metrics/data.js
+++ metrics/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1723141545784,
+  "lastUpdate": 1723151847468,
   "repoUrl": "https://github.com/oxc-project/monitor-oxc",
   "entries": {
     "Oxc Metrics": [
@@ -7856,6 +7856,38 @@ window.BENCHMARK_DATA = {
           {
             "name": "Compile Time",
             "value": 52.52,
+            "unit": "Seconds"
+          },
+          {
+            "name": "Binary Size",
+            "value": 4676304,
+            "unit": "Bytes"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Boshen",
+            "username": "Boshen",
+            "email": "boshenc@gmail.com"
+          },
+          "committer": {
+            "name": "Boshen",
+            "username": "Boshen",
+            "email": "boshenc@gmail.com"
+          },
+          "id": "5bd2790f694765be2aeda66c221c388103cc8f6c",
+          "message": "fix idempotency_test name",
+          "timestamp": "2024-08-06T06:33:10Z",
+          "url": "https://github.com/oxc-project/monitor-oxc/commit/5bd2790f694765be2aeda66c221c388103cc8f6c"
+        },
+        "date": 1723151846513,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Compile Time",
+            "value": 54.72,
             "unit": "Seconds"
           },
           {
