a2e6b3addcd827a78cdfa658542bdc5121b32b36
github-action-benchmark <github@users.noreply.github.com>
2024-08-12 06:26:08 +0000

    add Oxc Metrics (customSmallerIsBetter) benchmark result for 371f250537b238658bd8772f58190e14e9fe2af2

--- metrics/data.js
+++ metrics/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1723432853679,
+  "lastUpdate": 1723443968929,
   "repoUrl": "https://github.com/oxc-project/monitor-oxc",
   "entries": {
     "Oxc Metrics": [
@@ -8788,6 +8788,38 @@ window.BENCHMARK_DATA = {
           {
             "name": "Compile Time",
             "value": 53.07,
+            "unit": "Seconds"
+          },
+          {
+            "name": "Binary Size",
+            "value": 4684496,
+            "unit": "Bytes"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "renovate[bot]",
+            "username": "renovate[bot]",
+            "email": "29139614+renovate[bot]@users.noreply.github.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "371f250537b238658bd8772f58190e14e9fe2af2",
+          "message": "chore(deps): update npm packages",
+          "timestamp": "2024-08-11T16:35:24Z",
+          "url": "https://github.com/oxc-project/monitor-oxc/commit/371f250537b238658bd8772f58190e14e9fe2af2"
+        },
+        "date": 1723443968288,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Compile Time",
+            "value": 51.68,
             "unit": "Seconds"
           },
           {
