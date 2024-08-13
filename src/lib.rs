74f4d8472d7041114d6d8da8667a2aba24c94940
Boshen <boshenc@gmail.com>
2024-08-13 17:25:33 +0800

    ignore broken ts file

--- src/lib.rs
+++ src/lib.rs
@@ -19,6 +19,16 @@ use oxc::span::SourceType;
 use case::Case;
 use driver::Driver;
 
+const PATH_IGNORES: &[&str] = &[
+    "node_modules/.pnpm/node-domexception@1.0.0/node_modules/node-domexception/.history",
+    // intentional parse errors
+    "node_modules/.pnpm/thread-stream@3.1.0/node_modules/thread-stream/test/syntax-error.mjs",
+    "node_modules/.pnpm/pino@9.3.2/node_modules/pino/test/fixtures/syntax-error-esm.mjs",
+    "node_modules/.pnpm/charenc@0.0.2/node_modules/charenc/README.js",
+    // broken types
+    "node_modules/.pnpm/immer@10.1.1/node_modules/immer/src/types/types-internal.ts",
+];
+
 #[derive(Debug)]
 pub struct Diagnostic {
     pub case: &'static str,
@@ -42,14 +52,6 @@ impl Default for NodeModulesRunner {
         Self::new()
     }
 }
-
-const PATH_IGNORES: &[&str] = &[
-    "node_modules/.pnpm/node-domexception@1.0.0/node_modules/node-domexception/.history",
-    // intentional parse errors
-    "node_modules/.pnpm/thread-stream@3.1.0/node_modules/thread-stream/test/syntax-error.mjs",
-    "node_modules/.pnpm/pino@9.3.2/node_modules/pino/test/fixtures/syntax-error-esm.mjs",
-    "node_modules/.pnpm/charenc@0.0.2/node_modules/charenc/README.js",
-];
 
 impl NodeModulesRunner {
     pub fn new() -> Self {
@@ -97,11 +99,12 @@ impl NodeModulesRunner {
     }
 
     fn run_case(&self, case: &dyn Case) -> Result<(), Vec<Diagnostic>> {
-        println!("Running {}", case.name());
-        let diagnostics = self
-            .files
-            .iter()
-            .filter_map(|source| if let Err(d) = case.test(source) { Some(d) } else { None })
+        println!("Running {}.", case.name());
+        let results = self.files.iter().map(|source| case.test(source)).collect::<Vec<_>>();
+        println!("Ran {} times.", results.len());
+        let diagnostics = results
+            .into_iter()
+            .filter_map(|source| if let Err(d) = source { Some(d) } else { None })
             .collect::<Vec<_>>();
         if !diagnostics.is_empty() {
             return Err(diagnostics);
