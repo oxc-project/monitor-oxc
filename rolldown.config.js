e11566a68ab6b40cd6b097120ebdedd9b93203f3
Boshen <boshenc@gmail.com>
2024-08-28 14:21:23 +0800

    add rolldown

---
+++ rolldown.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'rolldown'
+
+let modulesCount = 0;
+
+export default defineConfig({
+  input: 'src/dynamic.test.mjs',
+  plugins: [
+    {
+      name: "counter",
+      transform() {
+        modulesCount += 1;
+      },
+      buildEnd() {
+        console.log("Total number of modules processed: " + modulesCount);
+      }
+    }
+  ]
+})
