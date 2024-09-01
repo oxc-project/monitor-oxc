11ba79fa99af7b773b6582ac00627a421e7507e3
Boshen <boshenc@gmail.com>
2024-09-01 21:36:02 +0800

    u

--- rolldown.config.js
+++ rolldown.config.js
@@ -3,22 +3,22 @@ import { defineConfig } from 'rolldown'
 let modulesCount = 0;
 
 export default defineConfig({
-  input: 'src/dynamic.test.mjs',
+  input: 'src/static.test.mjs',
   platform: 'node',
   // [UNLOADABLE_DEPENDENCY] Error: Could not load ... .node
   external: [
     "fsevents",
     "@swc/core"
   ],
-  plugins: [
-    {
-      name: "counter",
-      transform() {
-        modulesCount += 1;
-      },
-      buildEnd() {
-        console.log("Total number of modules processed: " + modulesCount);
-      }
-    }
-  ]
+  // plugins: [
+    // {
+      // name: "counter",
+      // transform() {
+        // modulesCount += 1;
+      // },
+      // buildEnd() {
+        // console.log("Total number of modules processed: " + modulesCount);
+      // }
+    // }
+  // ]
 })
