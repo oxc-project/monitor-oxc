7ea25c76e71841c796460d6e241747a51cba22b7
github-action-benchmark <github@users.noreply.github.com>
2024-09-01 03:25:44 +0000

    add Oxc Metrics (customSmallerIsBetter) benchmark result for cd757c9c1ef84059596477b7204f69c0575c2a6a

--- metrics/data.js
+++ metrics/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1725154797673,
+  "lastUpdate": 1725161144499,
   "repoUrl": "https://github.com/oxc-project/monitor-oxc",
   "entries": {
     "Oxc Metrics": [
@@ -15902,6 +15902,38 @@ window.BENCHMARK_DATA = {
           {
             "name": "Compile Time",
             "value": 52.58,
+            "unit": "Seconds"
+          },
+          {
+            "name": "Binary Size",
+            "value": 6093440,
+            "unit": "Bytes"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Boshen",
+            "username": "Boshen",
+            "email": "boshenc@gmail.com"
+          },
+          "committer": {
+            "name": "Boshen",
+            "username": "Boshen",
+            "email": "boshenc@gmail.com"
+          },
+          "id": "cd757c9c1ef84059596477b7204f69c0575c2a6a",
+          "message": "3000",
+          "timestamp": "2024-08-31T16:43:33Z",
+          "url": "https://github.com/oxc-project/monitor-oxc/commit/cd757c9c1ef84059596477b7204f69c0575c2a6a"
+        },
+        "date": 1725161143102,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Compile Time",
+            "value": 61.09,
             "unit": "Seconds"
           },
           {
