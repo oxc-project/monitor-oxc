a5b1c405b019fc14136c11f5b4acb20da80c340b
github-action-benchmark <github@users.noreply.github.com>
2024-09-13 03:21:28 +0000

    add Oxc Metrics (customSmallerIsBetter) benchmark result for bb4f7e5169455fec2b8576431dbfbcb186b1d61f

--- metrics/data.js
+++ metrics/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1726190554509,
+  "lastUpdate": 1726197688188,
   "repoUrl": "https://github.com/oxc-project/monitor-oxc",
   "entries": {
     "Oxc Metrics": [
@@ -20528,6 +20528,38 @@ window.BENCHMARK_DATA = {
           {
             "name": "Compile Time",
             "value": 53.51,
+            "unit": "Seconds"
+          },
+          {
+            "name": "Binary Size",
+            "value": 6236800,
+            "unit": "Bytes"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Boshen",
+            "username": "Boshen",
+            "email": "boshenc@gmail.com"
+          },
+          "committer": {
+            "name": "Boshen",
+            "username": "Boshen",
+            "email": "boshenc@gmail.com"
+          },
+          "id": "bb4f7e5169455fec2b8576431dbfbcb186b1d61f",
+          "message": "update renovate strategy",
+          "timestamp": "2024-09-12T01:22:07Z",
+          "url": "https://github.com/oxc-project/monitor-oxc/commit/bb4f7e5169455fec2b8576431dbfbcb186b1d61f"
+        },
+        "date": 1726197686009,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Compile Time",
+            "value": 54.07,
             "unit": "Seconds"
           },
           {
