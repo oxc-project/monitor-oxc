550b0f8cadaf48e319118455ffcac906d4e94f46
Boshen <boshenc@gmail.com>
2024-12-16 22:51:49 +0800

    update rolldown

--- rolldown.config.js
+++ rolldown.config.js
@@ -3,14 +3,19 @@ import { defineConfig } from 'rolldown'
 let modulesCount = 0;
 
 export default defineConfig({
-  input: 'src/dynamic.test.mjs',
+  input: 'src/static.test.mjs',
   platform: 'node',
   // [UNLOADABLE_DEPENDENCY] Error: Could not load ... .node
   external: [
     "fsevents",
     "@swc/core",
     "oxc-resolver",
+    "ssh2",
+    "@parcel/watcher",
   ],
+  resolve: {
+    extensions: [".js", ".cjs", ".mjs", ".json"]
+  },
   plugins: [
     {
       name: "counter",
